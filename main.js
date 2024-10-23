0a58d2e4d2250284ec0f4bff5e3cebadd25f5b2f
juliamensagem <105504640+juliamensagem@users.noreply.github.com>
2024-10-23 11:05:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const grafico = document.getElementById("meuGrafico").getContext("2d");
+const meuGrafico = new Chart (grafic, {
+    type: "pie",
+    data:{
+        labels:[
+            "Instagram",
+ "WhatsApp",
+ "Facebook",
+ "WeChat",
+ "TikTok",
+ "DouYin",
+ "Twitter",
+ "Telegram",
+ "FB Messenger",
+ "LINE",
+        ],
+    }
+}
+)
